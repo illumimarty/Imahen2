17be011614ad6a9d910a88cc5bef0d79804a0038
Marty Nodado (MBP) <mdrnodado@gmail.com>
2023-09-17 12:28:29 -0700

    Initial Commit

---
+++ Imahen2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Imahen2
+//
+//  Created by Marty Nodado on 9/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
